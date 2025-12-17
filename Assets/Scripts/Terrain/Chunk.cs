3ea1814213c306d8326391343ef231eaf27bbc47
xWalfieSMR <aaljalk223@g.educaand.es>
2025-12-17 13:44:46 +0100

    Added terrain generation (WIP)

---
+++ Assets/Scripts/Terrain/Chunk.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Terrain {
+    public class Chunk : MonoBehaviour {
+        [SerializeField] private int chunkWidth = 16;
+        
+        private const int WorldBottom = -64;
+        private const int WorldTop = 319;
+        private const int SeaLevel = 63;
+        
+        private static readonly int WorldHeight = WorldTop + Mathf.Abs(WorldBottom);
+
+        private byte[,,] _blocks;
+
+        private void Start() {
+            _blocks = new byte[chunkWidth, WorldHeight, chunkWidth];
+            Generate();
+            BuildMesh();
+        }
+        
+        private void Generate() {
+            for (var x = 0; x < chunkWidth; x++) {
+                for (var y = 0; y < WorldHeight; y++) {
+                    for (var z = 0; z < chunkWidth; z++) {
+                        var height = GetTerrainHeight(x, z);
+                        
+                        var worldY = y + WorldBottom;
+
+                        if (worldY < height) {
+                            // Solid terrain
+                            _blocks[x, y, z] = GetBlockType(worldY, height);
+                        } else if (worldY < SeaLevel) {
+                            _blocks[x, y, z] = (byte)BlockType.Water;
+                        } else { _blocks[x, y, z] = (byte)BlockType.Air; }
+                    }
+                }
+            }
+        }
+        
+        private void BuildMesh() {
+            // Lists to hold mesh data
+            var vertices = new List<Vector3>();
+            var triangles = new List<int>();
+
+            // Loop through each block
+            for (var x = 0; x < chunkWidth; x++) {
+                for (var y = 0; y < WorldHeight; y++) {
+                    for (var z = 0; z < chunkWidth; z++) {
+                        // Skip air (0)
+                        if (_blocks[x, y, z] == 0) continue;
+                        
+                        // Check each direction for a neighbor air block
+                        if (y == WorldHeight - 1 || _blocks[x, y + 1, z] == 0) {
+                            // Add top face
+                            var vertIndex = vertices.Count;
+
+                            // Each corner
+                            vertices.Add(new Vector3(x, y + 1, z));         // bottom-left
+                            vertices.Add(new Vector3(x + 1, y + 1, z));     // bottom-right
+                            vertices.Add(new Vector3(x + 1, y + 1, z + 1)); // top-right
+                            vertices.Add(new Vector3(x, y + 1, z + 1));     // top-left
+
+                            // Each triangle
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 2);
+                            triangles.Add(vertIndex + 1);
+
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 3);
+                            triangles.Add(vertIndex + 2);
+                        }
+                        if (y == 0 || _blocks[x, y - 1, z] == 0) {
+                            // Add bottom face
+                            var vertIndex = vertices.Count;
+
+                            // Each corner
+                            vertices.Add(new Vector3(x, y, z));         // bottom-left
+                            vertices.Add(new Vector3(x + 1, y, z));     // bottom-right
+                            vertices.Add(new Vector3(x + 1, y, z + 1)); // top-right
+                            vertices.Add(new Vector3(x, y, z + 1));     // top-left
+
+                            // Each triangle
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 1);
+                            triangles.Add(vertIndex + 2);
+
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 2);
+                            triangles.Add(vertIndex + 3);
+                        }
+                        if (z == chunkWidth - 1 || _blocks[x, y, z + 1] == 0) {
+                            // Add north face
+                            var vertIndex = vertices.Count;
+
+                            // Each corner
+                            vertices.Add(new Vector3(x, y, z + 1));         // bottom-left
+                            vertices.Add(new Vector3(x + 1, y, z + 1));     // bottom-right
+                            vertices.Add(new Vector3(x + 1, y + 1, z + 1)); // top-right
+                            vertices.Add(new Vector3(x, y + 1, z + 1));     // top-left
+
+                            // Each triangle
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 1);
+                            triangles.Add(vertIndex + 2);
+
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 2);
+                            triangles.Add(vertIndex + 3);
+                        }
+                        if (z == 0 || _blocks[x, y, z - 1] == 0) {
+                            // Add south face
+                            var vertIndex = vertices.Count;
+
+                            // Each corner
+                            vertices.Add(new Vector3(x, y, z));             // bottom-left
+                            vertices.Add(new Vector3(x + 1, y, z));         // bottom-right
+                            vertices.Add(new Vector3(x + 1, y + 1, z));     // top-right
+                            vertices.Add(new Vector3(x, y + 1, z));         // top-left
+
+                            // Each triangle
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 2);
+                            triangles.Add(vertIndex + 1);
+
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 3);
+                            triangles.Add(vertIndex + 2);
+                        }
+                        if (x == chunkWidth - 1 || _blocks[x + 1, y, z] == 0) {
+                            // Add east face
+                            var vertIndex = vertices.Count;
+
+                            // Each corner
+                            vertices.Add(new Vector3(x + 1, y, z));         // bottom-left
+                            vertices.Add(new Vector3(x + 1, y, z + 1));     // bottom-right
+                            vertices.Add(new Vector3(x + 1, y + 1, z + 1)); // top-right
+                            vertices.Add(new Vector3(x + 1, y + 1, z));     // top-left
+
+                            // Each triangle
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 2);
+                            triangles.Add(vertIndex + 1);
+
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 3);
+                            triangles.Add(vertIndex + 2);
+                        }
+                        if (x == 0 || _blocks[x - 1, y, z] == 0) {
+                            // Add west face
+                            var vertIndex = vertices.Count;
+
+                            // Each corner
+                            vertices.Add(new Vector3(x, y, z));             // bottom-left
+                            vertices.Add(new Vector3(x, y, z + 1));         // bottom-right
+                            vertices.Add(new Vector3(x, y + 1, z + 1));     // top-right
+                            vertices.Add(new Vector3(x, y + 1, z));         // top-left
+
+                            // Each triangle
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 1);
+                            triangles.Add(vertIndex + 2);
+
+                            triangles.Add(vertIndex + 0);
+                            triangles.Add(vertIndex + 2);
+                            triangles.Add(vertIndex + 3);
+                        }
+                    }
+                }
+            }
+            
+            
+            
+            var mesh = new Mesh {
+                // Convert to array
+                vertices = vertices.ToArray(),
+                triangles = triangles.ToArray()
+            };
+            
+            mesh.RecalculateNormals();
+
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
+
+        private static float GetTerrainHeight(int x, int z) {
+            // Base terrain (bedrock to sea level)
+            var baseNoise = Mathf.PerlinNoise(x * 0.1f, z * 0.1f);
+            var baseHeight = baseNoise * (SeaLevel - WorldBottom) + WorldBottom;
+    
+            // Terrain variation above sea level (plains, hills, mountains)
+            var terrainNoise = Mathf.PerlinNoise(x * 0.05f, z * 0.05f);
+            var terrainVariation = terrainNoise * (WorldTop - SeaLevel);
+    
+            return baseHeight + terrainVariation;
+        }
+
+        private static byte GetBlockType(int worldY, float terrainHeight) {
+            // Get block type from depth
+            var depthFromSurface = terrainHeight - worldY;
+
+            return depthFromSurface switch {
+                < 1 => (byte)BlockType.Grass,
+                < 4 => (byte)BlockType.Dirt,
+                _ => (byte)BlockType.Stone
+            };
+        }
+    }
+
+    public enum BlockType {
+        Air = 0,
+        Stone = 1,
+        Water = 2,
+        Grass = 3,
+        Dirt = 4
+    }
+}
---
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Terrain {
+    public class TerrainGenerator : MonoBehaviour {
+        public Vector2Int gridSize;
+
+        private void Start() {
+            var vertices = new Vector3[gridSize.x * gridSize.y];
+
+            var i = 0;
+            for (var z = 0; z < gridSize.y; z++) {
+                for (var x = 0; x < gridSize.x; x++) {
+                    var y = Mathf.Round(Mathf.PerlinNoise(x * 0.1f, z * 0.1f) * 5f);
+                    vertices[i] = new Vector3(x, y, z);
+                    
+                    i++;
+                }
+            }
+            
+            var triangles = new int[(gridSize.x - 1) * (gridSize.y - 1) * 6];
+
+            var vert = 0;
+            var tris = 0;
+            for (var y = 0; y < gridSize.y - 1; y++) {
+                for (var x = 0; x < gridSize.x - 1; x++) {
+                    triangles[tris + 0] = vert + 0;
+                    triangles[tris + 1] = vert + 1;
+                    triangles[tris + 2] = vert + gridSize.x;
+                    
+                    triangles[tris + 3] = vert + 1;
+                    triangles[tris + 4] = vert + gridSize.x + 1;
+                    triangles[tris + 5] = vert + gridSize.x;
+
+                    vert++;
+                    tris += 6;
+                }
+                vert++;
+            }
+            
+            var mesh = new Mesh {
+                vertices = vertices,
+                triangles = triangles
+            };
+            
+            mesh.RecalculateNormals();
+            
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
+    }
+}
